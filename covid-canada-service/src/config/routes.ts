0181b18d01e6244625a56d0e977432b521d68e2d
Jason Dippel <dippelj12@gmail.com>
2021-02-03 19:24:04 -0800

    add liveness and readiness checks

--- covid-canada-service/src/config/routes.ts
+++ covid-canada-service/src/config/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import RootRoutes from '../routes/root'
 import ProvincesRoutes from '../routes/provinces'
 import RegionsRoutes from '../routes/regions'
+import HealthRoutes from '../routes/health'
 
 const router = Router()
 
 router.use('/', RootRoutes)
 router.use('/provinces', ProvincesRoutes)
 router.use('/regions', RegionsRoutes)
+router.use('/health', HealthRoutes)
 
 export default router
